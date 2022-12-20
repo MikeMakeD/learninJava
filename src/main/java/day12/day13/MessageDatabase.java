2a0d25481cacf3130bae8b9391e9e6d4151c3956
MikeMakeD <you@example.com>
2022-12-20 10:12:15 +0700

    added days 13

---
+++ src/main/java/day12/day13/MessageDatabase.java
@@ -0,0 +1,24 @@
+package day12.day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageDatabase {
+    static List<Message> messages = new ArrayList<Message>();
+
+    public static void addNewMessage (User u1, User u2, String text){
+        messages.add(new Message(u1,u2,text));
+    }
+
+    public static List<Message> getMessages(){
+        return messages;
+    }
+
+    public static void showDialog(User u1, User u2){
+        for (Message message: messages) {
+            if(message.getSender()==u1&& message.getReceiver()==u2|| message.getReceiver()==u1 &&message.getSender()==u2){
+                System.out.println(message.getSender()+": "+ message.getText());
+            }
+        }
+    }
+}
---
+++ src/main/java/day12/day13/Task1.java
@@ -0,0 +1,25 @@
+package day12.day13;
+
+public class Task1 {
+    public static void main(String[] args) {
+        User user1 = new User("Боб");
+        User user2 = new User("Сем");
+        User user3 = new User("Джон");
+
+        user1.sendMessage(user2,"Привет, Сем");
+        user1.sendMessage(user2,"Как твои дела?");
+        user2.sendMessage(user1,"Првиет, Боб");
+        user2.sendMessage(user1,"У меня все хорошо!");
+        user2.sendMessage(user1,"Как твои дела?");
+        user3.sendMessage(user1,"Привет, Боб!");
+        user3.sendMessage(user1,"Как твои дела?");
+        user3.sendMessage(user1,"Как твое здоровье?");
+        user1.sendMessage(user3,"Привет, Джон");
+        user1.sendMessage(user3,"У меня все отлично");
+        user1.sendMessage(user3,"Как твои дела?");
+        user3.sendMessage(user1,"У меня тоже все отлично");
+
+        MessageDatabase.showDialog(user1,user3);
+
+    }
+}
